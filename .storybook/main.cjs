6b62e69f03e5787f965db25d218bad587f1bac36
mr-cerebro <mrcerebro8@gmail.com>
2022-10-17 22:21:44 +0100

    update main.cjs scripts

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -15,6 +15,7 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
+  staticDirs: ["../public"],
   viteFinal: (config, { configType }) => {
     if(configType === "PRODUCTION") {
       config.base = "/ignite-lab-design-system/"
